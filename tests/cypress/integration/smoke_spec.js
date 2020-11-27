2f3af60e7a93133f8f8311f8306a15e4ed9e8801
Christoffer G. Thomsen <chris@cgt.name>
2020-11-27 20:07:01 +0100

    Configure Cypress base URL
    For local development

--- tests/cypress/integration/smoke_spec.js
+++ tests/cypress/integration/smoke_spec.js
@@ -1,6 +1,6 @@
 describe('The website', () => {
     it('loads', () => {
-        cy.visit('http://localhost:8080');
+        cy.visit('/');
         cy.contains('Connected to backend: It works!');
     });
 });
