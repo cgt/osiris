2e507a61b1e4c42a912f1ca1dc6b0f609dabe0cd
Christoffer G. Thomsen <chris@cgt.name>
2020-12-19 14:21:03 +0100

    Remove backend connection check from frontend
    I've already seen that it works. Now it's just in the way.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
-    const [message, setMessage] = useState('Awaiting communication with backend');
-    useEffect(() => {
-        fetch('/api/it-works')
-            .then(res => {
-                if (res.status === 418) {
-                    return res.text().then(message => {
-                        setMessage(`Connected to backend: ${message}`);
-                    });
-                } else {
-                    setMessage(`Unexpected response from backend: ${res.statusText}`);
-                }
-            })
-            .catch(err => {
-                setMessage(`Error connecting to backend: ${err}`);
-            });
-    }, []);
     return (
-        <p>{message}</p>
+        <p>Hello, World!</p>
     );
 }
 
