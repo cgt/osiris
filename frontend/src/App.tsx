c729275951750a50ecebf95bb347e247e2e6ef82
Christoffer G. Thomsen <chris@cgt.name>
2020-12-19 16:00:27 +0100

    Show non-functional sign up form
    Adapted from
    https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/sign-up/SignUp.js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Button, Container, Grid, makeStyles, TextField, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(3),
+    },
+}));
 
 function App() {
+    const classes = useStyles();
+
     return (
-        <Container>
-            <p>Hello, World!</p>
+        <Container component="main" maxWidth="xs">
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                autoFocus
+                                variant="outlined"
+                                name="username"
+                                label="Username"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                variant="outlined"
+                                type="password"
+                                name="password"
+                                label="Password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                            >
+                                Sign up
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
         </Container>
     );
 }
