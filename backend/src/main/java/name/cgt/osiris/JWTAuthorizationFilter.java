9f8bf250e90de70ff47bc6c9f2ad96d7ab262f54
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 14:35:57 +0100

    Extract field

--- backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
+++ backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
@@ -15,6 +15,12 @@ import java.io.IOException;
 import java.util.List;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    private final Algorithm secret;
+
+    public JWTAuthorizationFilter() {
+        secret = Algorithm.HMAC512("DUMMY SECRET");
+    }
+
     @Override
     protected void doFilterInternal(
       HttpServletRequest request,
@@ -24,7 +30,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         final var authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             final var token = authorizationHeader.replace("Bearer ", "");
-            final var jwt = JWT.require(Algorithm.HMAC512("DUMMY SECRET")).build();
+            final var jwt = JWT.require(secret).build();
             final var username = jwt.verify(token).getSubject();
             if (username != null) {
                 final var auth = new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
