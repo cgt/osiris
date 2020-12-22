0bb0b03b21df71b5482bea75c05aa59677e685c9
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 11:34:07 +0100

    Extend JWTAuthorizationFilter from simpler base class
    The Auth0 blog post about implementing JWT auth for Spring Boot[0]
    that I adapted my implemention from used BasicAuthenticationFilter, so
    that's what I did, too. It seemed weird to me, so I poked around and it
    doesn't seem to be necessary. Extending OncePerRequestFilter (which is
    the base class for BasicAuthenticationFilter) works just fine.
    
    The only problem was that Spring started complaining that it didn't know
    what order to run the JWTAuthorizationFilter in, hence the change to
    SecurityConfiguration. I somewhat arbitrarily chose to make it run after
    BasicAuthenticationFilter.
    
    0: https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot

--- backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
+++ backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
@@ -2,11 +2,10 @@ package name.cgt.osiris;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -15,11 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
-    public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
-        super(authenticationManager);
-    }
-
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
       HttpServletRequest request,
--- backend/src/main/java/name/cgt/osiris/SecurityConfiguration.java
+++ backend/src/main/java/name/cgt/osiris/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -25,7 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
           .antMatchers("/api/login").permitAll()
           .antMatchers("/api/**").authenticated()
           .and()
-          .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+          .addFilterAfter(new JWTAuthorizationFilter(), BasicAuthenticationFilter.class)
           .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
