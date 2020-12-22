c91893e62ef50bbc116190c9d9a1041ae30e85c3
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 16:10:44 +0100

    Add explicit @Import for otherwise excluded class in a @WebMvcTest
    It's not necessary yet, but it will be once we inject TokenIssuer into
    LoginController instead of constructing it.

--- backend/src/test/java/name/cgt/osiris/AuthenticationTest.java
+++ backend/src/test/java/name/cgt/osiris/AuthenticationTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@Import(TokenIssuer.class)
 public class AuthenticationTest {
     @Autowired
     private MockMvc mvc;
