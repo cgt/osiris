3fc1f89ecff689ddbf37922f3db7cb4d16b42033
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 17:02:49 +0100

    Add redundant condition

--- backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
+++ backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
@@ -36,7 +36,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             final var username = jwt.verify(token).getSubject();
             final var optionalUsername = Optional.ofNullable(username);
             var auth = Optional.<Authentication>empty();
-            if (username != null) {
+            if (username != null && optionalUsername.isPresent()) {
                 auth = Optional.of(new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority("ROLE_USER"))));
             }
             auth.ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
