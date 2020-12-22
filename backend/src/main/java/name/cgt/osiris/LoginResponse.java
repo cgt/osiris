6ad7b1498e8fb1822baf5798bb806d89c83a1da4
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 11:00:37 +0100

    Use Lombok for DTO boilerplate

--- backend/src/main/java/name/cgt/osiris/LoginResponse.java
+++ backend/src/main/java/name/cgt/osiris/LoginResponse.java
@@ -1,36 +1,12 @@
 package name.cgt.osiris;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponse {
     private String token;
-
-    @SuppressWarnings("unused")
-    public LoginResponse() {
-    }
-
-    public LoginResponse(String token) {
-        this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LoginResponse that = (LoginResponse) o;
-        return token.equals(that.token);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(token);
-    }
 }
