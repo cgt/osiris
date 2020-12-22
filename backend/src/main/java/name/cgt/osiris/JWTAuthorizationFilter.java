f3737f660fd27c78160274c7cc5a7f435368e6d4
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 14:51:20 +0100

    Extract field

--- backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
+++ backend/src/main/java/name/cgt/osiris/JWTAuthorizationFilter.java
@@ -1,6 +1,7 @@
 package name.cgt.osiris;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,9 +17,11 @@ import java.util.List;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
     private final Algorithm jwtSigner;
+    private final JWTVerifier jwt;
 
     public JWTAuthorizationFilter(Algorithm jwtSigner) {
         this.jwtSigner = jwtSigner;
+        jwt = JWT.require(jwtSigner).build();
     }
 
     @Override
@@ -30,7 +33,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         final var authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             final var token = authorizationHeader.replace("Bearer ", "");
-            final var jwt = JWT.require(jwtSigner).build();
+            final var jwt = this.jwt;
             final var username = jwt.verify(token).getSubject();
             if (username != null) {
                 final var auth = new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
