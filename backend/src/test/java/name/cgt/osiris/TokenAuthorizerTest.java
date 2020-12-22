9a249779ce122b5fc9b90f3db06642073bc0cd1c
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 18:09:32 +0100

    Check that valid tokens are authorized

--- backend/src/test/java/name/cgt/osiris/TokenAuthorizerTest.java
+++ backend/src/test/java/name/cgt/osiris/TokenAuthorizerTest.java
@@ -4,10 +4,13 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +19,21 @@ public class TokenAuthorizerTest {
     private final Algorithm signingAlgorithm = Algorithm.none();
     private final JWTVerifier jwtVerifier = JWT.require(signingAlgorithm).build();
     private final TokenAuthorizer authorizer = new TokenAuthorizer(jwtVerifier);
+
+    @Test
+    public void valid_token_is_authorized() {
+        final var validToken =
+          JWT
+            .create()
+            .withSubject("username")
+            .withExpiresAt(farInTheFuture())
+            .sign(signingAlgorithm);
+
+        final var authentication = authorizer.authFromHeader("Bearer " + validToken);
+
+        assertThat(authentication)
+          .hasValue(new UsernamePasswordAuthenticationToken("username", null, List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+    }
 
     @Test
     public void token_without_username_is_unauthorized() {
