94fd23d518f07bfd8592addefde79ef4cdc6b413
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 15:55:17 +0100

    Make field non-final in preparation for Extract Delegate

--- backend/src/main/java/name/cgt/osiris/LoginController.java
+++ backend/src/main/java/name/cgt/osiris/LoginController.java
@@ -20,7 +20,7 @@ import java.util.Date;
 @RequestMapping("/api/login")
 public class LoginController {
     private final AuthenticationManager authManager;
-    private final Algorithm jwtSigner;
+    private Algorithm jwtSigner;
 
     public LoginController(AuthenticationManager authManager, Algorithm jwtSigner) {
         this.authManager = authManager;
