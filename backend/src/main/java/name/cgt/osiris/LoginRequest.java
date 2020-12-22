cbb543fe4705b88e6125a871d12f9e3b08bf311d
Christoffer G. Thomsen <chris@cgt.name>
2020-12-22 11:48:16 +0100

    Mark fields on DTO received from client as @Nullable
    You never know what you're going to get from the horrible outside world.

--- backend/src/main/java/name/cgt/osiris/LoginRequest.java
+++ backend/src/main/java/name/cgt/osiris/LoginRequest.java
@@ -1,9 +1,13 @@
 package name.cgt.osiris;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
 
 @Data
 public class LoginRequest {
+    @Nullable
     private String username;
+
+    @Nullable
     private String password;
 }
